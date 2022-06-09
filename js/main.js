e8d8e623430cff431cdd7920232b3790cb07c225
jun-isaac <100373800+jun-isaac@users.noreply.github.com>
2022-06-09 12:37:28 +0900

    이미지 갤러리
    이미지 변경 연습

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.img-thumb li').click(function () {
+        // this 부모 li 중 사용자가 클릭한 li
+        // 그 밑에 이미지 src값 읽음
+        var thisSrc = $(this).children().attr('src');
+        console.log(thisSrc)
+        // 메인이미지 변경
+        $('.main-image img').attr('src', thisSrc);
+
+    });
+
+});
